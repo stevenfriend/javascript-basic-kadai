aab575dce1eac1890b1d87ae179a68fad0ebed23
Steven Friend <stevenmarkfriend@gmail.com>
2023-12-30 11:40:59 +0900

    課題２１の追加

---
+++ kadai_021/event.js
@@ -0,0 +1,8 @@
+const text = document.getElementById('text');
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+  setTimeout(() => {
+    text.innerText = 'ボタンをクリックしました';
+  }, 2000);
+})
